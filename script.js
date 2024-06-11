1f4dd07d265ec6d2023a215e0d3dfaf90f67dc5f
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-06-11 05:27:37 +0000

    Increase the number of buckets if there are more keys than the load factor percentage of buckets

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ import LinkedList from "./linkedList.js";
 
 class HashMap {
     constructor() {
-        this.nrOfBuckets = 3;
+        this.nrOfBuckets = 1;
         this.loadFactor = 0.75;
         this.arr = new Array(this.nrOfBuckets)
+        this.nrOfKeys = 0;
     }
     
     hash(key) { //1
@@ -24,15 +25,26 @@ class HashMap {
     set(key, value) {
         let currentIndex = this.hash(key);
         let tempObj = {key: key, value:value};
+
+        if (this.nrOfKeys > (this.nrOfBuckets * this.loadFactor)) {
+            this.nrOfBuckets *= 2; 
+            console.log(this.nrOfBuckets + "bucketz")
+        }
    
-        if (this.arr[currentIndex] == undefined || this.arr[currentIndex].key == tempObj.key) {
+        if (this.arr[currentIndex] == undefined) {
+            this.arr[currentIndex] = tempObj;  
+            this.nrOfKeys += 1;
+            console.log(this.nrOfKeys)
+        } else if (this.arr[currentIndex].key == tempObj.key) {
             this.arr[currentIndex] = tempObj;    
         } else {
             let test = new LinkedList;
-            console.log(this.arr[currentIndex])
+            // console.log(this.arr[currentIndex])
             test.append((this.arr[currentIndex].key), (this.arr[currentIndex].value))
             test.append(tempObj.key, tempObj.value)
             this.arr[currentIndex] = test
+            this.nrOfKeys += 1;
+            console.log(this.nrOfKeys)
         }
     }
 }
